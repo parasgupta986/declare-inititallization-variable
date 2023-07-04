7ce60ea2dbe74a8dd7a181eb543e6ea0a677d171
paras gupta <pg9080986@gmail.com>
2023-07-04 18:03:14 +0530

    Initial commit

---
+++ src/com/intershala/javaapp/Main.java
@@ -0,0 +1,11 @@
+package com.intershala.javaapp;
+
+public class Main {
+
+    public static void main(String[] args) {
+	int age=26;
+	String name =" raghav ";
+	double height=1.75;
+	    System.out.println("Name :- "+name+"  Age="+age+" Height:--"+ height);
+    }
+}
